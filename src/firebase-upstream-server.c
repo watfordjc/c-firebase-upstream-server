4e19c96d6634d773cdc0d9c508a43772a14a522e
John Cook <github-2014-04-13@johncook.co.uk>
2019-02-25 23:17:32 +0000

    Remove unneeded message and version handlers

--- src/firebase-upstream-server.c
+++ src/firebase-upstream-server.c
@@ -68,52 +68,6 @@ unsigned short get_port(int port);
 void servers_iterate();
 void logins_iterate(int serverInt, struct config_settings server_login, struct config_setting_t *server_element);
 
-int version_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, void * const userdata)
-{
-  xmpp_stanza_t *reply, *query, *name, *version, *text;
-  const char *ns;
-  xmpp_ctx_t *ctx = (xmpp_ctx_t*)userdata;
-
-  printf("Received version request from %s\n", xmpp_stanza_get_from(stanza));
-
-  reply = xmpp_stanza_reply(stanza);
-  xmpp_stanza_set_type(reply, "result");
-
-  query = xmpp_stanza_new(ctx);
-  xmpp_stanza_set_name(query, "query");
-  ns = xmpp_stanza_get_ns(xmpp_stanza_get_children(stanza));
-  if (ns) {
-    xmpp_stanza_set_ns(query, ns);
-  }
-
-  name = xmpp_stanza_new(ctx);
-  xmpp_stanza_set_name(name, "name");
-  xmpp_stanza_add_child(query, name);
-  xmpp_stanza_release(name);
-
-  text = xmpp_stanza_new(ctx);
-  xmpp_stanza_set_text(text, "libstrophe example bot");
-  xmpp_stanza_add_child(name, text);
-  xmpp_stanza_release(text);
-
-  version = xmpp_stanza_new(ctx);
-  xmpp_stanza_set_name(version, "version");
-  xmpp_stanza_add_child(query, version);
-  xmpp_stanza_release(version);
-
-  text = xmpp_stanza_new(ctx);
-  xmpp_stanza_set_text(text, "1.0");
-  xmpp_stanza_add_child(version, text);
-  xmpp_stanza_release(text);
-
-  xmpp_stanza_add_child(reply, query);
-  xmpp_stanza_release(query);
-
-  xmpp_send(conn, reply);
-  xmpp_stanza_release(reply);
-  return 1;
-}
-
 /* OpenSSL function declarations for cleanup */
 int FIPS_mode_set();
 void CRYPTO_set_locking_callback();
@@ -238,54 +192,6 @@ int fcm_upstream_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza,
   return 1;
 }
 
-int message_handler(xmpp_conn_t * const conn, xmpp_stanza_t * const stanza, void * const userdata)
-{
-  xmpp_ctx_t *ctx = (xmpp_ctx_t*)userdata;
-  xmpp_stanza_t *body, *reply;
-  const char *type;
-  char *intext, *replytext;
-  int quit = 0;
-
-  body = xmpp_stanza_get_child_by_name(stanza, "body");
-  if (body == NULL) {
-    return 1;
-  }
-  type = xmpp_stanza_get_type(stanza);
-  if (type != NULL && strcmp(type, "error") == 0) {
-    return 1;
-  }
-
-  intext = xmpp_stanza_get_text(body);
-
-  printf("Incoming message from %s: %s\n", xmpp_stanza_get_from(stanza), intext);
-
-  reply = xmpp_stanza_reply(stanza);
-  if (xmpp_stanza_get_type(reply) == NULL) {
-    xmpp_stanza_set_type(reply, "chat");
-  }
-
-  if (strcmp(intext, "quit") == 0) {
-    replytext = strdup("bye!");
-    quit = 1;
-  } else {
-    replytext = (char *) malloc(strlen(" to you too!") + strlen(intext) + 1);
-    strcpy(replytext, intext);
-    strcat(replytext, " to you too!");
-  }
-  xmpp_free(ctx, intext);
-  xmpp_message_set_body(reply, replytext);
-
-  xmpp_send(conn, reply);
-  xmpp_stanza_release(reply);
-  free(replytext);
-
-  if (quit) {
-    xmpp_disconnect(conn);
-  }
-
-  return 1;
-}
-
 /* define a handler for connection events */
 void conn_handler(xmpp_conn_t * const conn, const xmpp_conn_event_t status,
   const int error, xmpp_stream_error_t * const stream_error,
@@ -296,9 +202,7 @@ void conn_handler(xmpp_conn_t * const conn, const xmpp_conn_event_t status,
   if (status == XMPP_CONN_CONNECT) {
     xmpp_stanza_t* pres;
     fprintf(stderr, "DEBUG: CONNECTED\n");
-    xmpp_handler_add(conn, version_handler, "jabber:iq:version", "iq", NULL, ctx);
     xmpp_handler_add(conn, fcm_upstream_handler, "google:mobile:data", "message", NULL, ctx);
-    xmpp_handler_add(conn, message_handler, NULL, "message", NULL, ctx);
 
   } else {
     fprintf(stderr, "DEBUG: DISCONNECTED\n");
