a402a29d874053830a4f62e7deccadb0705dd176
John Cook <github-2014-04-13@johncook.co.uk>
2019-03-08 07:07:09 +0000

    Store pointers to pthread_mutex_t and pthread_cond_t in struct

--- src/common.h
+++ src/common.h
@@ -3,11 +3,11 @@ struct login_settings
   unsigned short port;
   int enabled;
   long flags;
+  struct config_setting_t *pointer;
+  struct connection_pair *pairs;
   const char *host;
   const char *jid;
   const char *pass;
-  struct config_setting_t *pointer;
-  struct connection_pair *pairs;
 };
 
 struct config_pointer
